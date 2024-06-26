8e49a48ed42d2b0db5b3da1f28f7eb61cbd9174f
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-06-26 19:41:55 +0900

    Add main

--- steps/step06.py
+++ steps/step06.py
@@ -37,3 +37,18 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
+    
+A = Square()
+B = Exp()
+C = Square()
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
+
