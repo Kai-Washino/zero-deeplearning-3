f839322dbb660c0a7bf606e472cd76fd18ae6287
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-08-16 17:02:47 +0900

    Add reshape

--- dezero/core.py
+++ dezero/core.py
@@ -1,6 +1,7 @@
 import numpy as np
 import weakref
 import contextlib
+import dezero
 
 # =============================================================================
 # Config
@@ -111,6 +112,11 @@ class Variable:
             if not retain_grad:
                 for y in f.outputs:
                     y().grad = None  # y is weakref
+        
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
 
 
 def as_variable(obj):
